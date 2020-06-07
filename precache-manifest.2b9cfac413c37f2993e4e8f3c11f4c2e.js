fecb36e3b32f047b84670bf5e18915bbb6bd86ee
ziveo <bziveo@gmail.com>
2020-06-07 23:00:18 +0000

    Deploying to gh-pages from master 9399267eb19eb53c80ad41933866a28fe7e6f869

---
+++ precache-manifest.2b9cfac413c37f2993e4e8f3c11f4c2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aef5f297db61a1001e0c4bdb93fce77",
+    "url": "./index.html"
+  },
+  {
+    "revision": "76195b34624b766da212",
+    "url": "./static/css/main.d09490d6.chunk.css"
+  },
+  {
+    "revision": "6d0cde58cb65372cc515",
+    "url": "./static/js/2.0b8d0548.chunk.js"
+  },
+  {
+    "revision": "cf2af42c1ffcb423ffd45b841a2a677c",
+    "url": "./static/js/2.0b8d0548.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76195b34624b766da212",
+    "url": "./static/js/main.0d65909d.chunk.js"
+  },
+  {
+    "revision": "ac300b647aa659044292",
+    "url": "./static/js/runtime-main.374f8d5c.js"
+  }
+]);
